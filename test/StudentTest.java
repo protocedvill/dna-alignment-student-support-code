6aa14b1ced9f85e9a015b391f55628d9f9381a88
Tianyu Chen <chentianyu@outlook.com>
2023-11-27 11:21:30 -0500

    import

---
+++ test/StudentTest.java
@@ -0,0 +1,16 @@
+/**
+ * To test with JUnit, add JUnit to your project. To do this, go to
+ * Project->Properties. Select "Java Build Path". Select the "Libraries"
+ * tab and "Add Library". Select JUnit, then JUnit 4.
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentTest {
+  @Test
+  public void test() {
+    // your tests go here
+  }
+
+}
